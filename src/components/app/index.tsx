cde9fbb05b80f0b4699ffe8699ad7217498533e8
Alexander Ilinykh <ilinykh_ab@sima-land.ru>
2022-05-25 13:22:43 +0500

    #1 installed sass and moved App into components folder

---
+++ src/components/app/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "./logo.svg";
+import styles from "./app.module.scss";
+
+function App() {
+  return <div className={styles.app}>APP</div>;
+}
+
+export default App;
